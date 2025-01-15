413ef79fd16378abc3b2309b1247dffe02bd0304
h3rx4 <lincolnmorden@gmail.com>
2025-01-14 19:48:04 -0500

    mn

--- src/main/java/net/h3rx4/brickables/block/ModBlocks.java
+++ src/main/java/net/h3rx4/brickables/block/ModBlocks.java
@@ -1,20 +1,32 @@
 package net.h3rx4.brickables.block;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
 import net.h3rx4.brickables.Brickables;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
 
 public class ModBlocks {
+    public static final Block CALCITE_BRICKS = registerBlock("calcite_bricks",
+            new Block(AbstractBlock.Settings.create().strength(1f)
+                    .requiresTool().sounds(BlockSoundGroup.CALCITE)));
 
 
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+         return Registry.register(Registries.BLOCK, Identifier.of(Brickables.MOD_ID, name ),block);
+    }
 
+ private static void registerBlockItem(String name, Block block) {
+     Registry.register(Registries.ITEM, Identifier.of(Brickables.MOD_ID, name),
+             new BlockItem(block, new Item.Settings()));
+ }
 
     public static void registerModBlocks() {
-        Brickables.LOGGER.debug("Registering ModBlocks for " + Brickables.MOD_ID);
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entires -> {
-
-        });
-
+        Brickables.LOGGER.info("Registering Mod Blocks for " + Brickables.MOD_ID);
     }
 }
--- src/main/java/net/h3rx4/brickables/item/ModItems.java
+++ src/main/java/net/h3rx4/brickables/item/ModItems.java
@@ -1,10 +1,17 @@
 package net.h3rx4.brickables.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.h3rx4.brickables.Brickables;
+import net.h3rx4.brickables.block.ModBlocks;
+import net.minecraft.item.ItemGroups;
 
 public class ModItems {
 
     public static void registerModItems() {
         Brickables.LOGGER.debug("Registering Mod Items for " + Brickables.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ModBlocks.CALCITE_BRICKS);
+        });
     }
 }
